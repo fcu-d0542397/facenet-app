2b7e628fe93c6e335c33c2dd62c13434f8fd2d51
david tsai <david90103@gmail.com>
2019-06-27 22:22:43 +0800

    edit register page

--- lib/cameraTutorial.dart
+++ lib/cameraTutorial.dart
@@ -69,18 +69,23 @@ class CameraTutorialPage extends StatelessWidget {
 
   Widget _tutorialBody(BuildContext context) {
     return Container(
-        alignment: Alignment.center,
-        padding: EdgeInsets.symmetric(horizontal: 40),
+        alignment: Alignment.topCenter,
+        padding: EdgeInsets.all(40),
         child: new RichText(
           text: new TextSpan(
             style: new TextStyle(
               fontSize: 18,
               height: 1.3,
-              color: Colors.black,
+              color: Colors.grey[700],
             ),
             children: <TextSpan>[
-              new TextSpan(text: '將開始進行臉部照片擷取\n'),
-              new TextSpan(text: '請將臉部對齊標線後\n連續拍攝30張臉部照片'),
+              new TextSpan(text: '將開始進行臉部照片擷取\n請將臉部對齊標線後\n連續拍攝30張臉部照片\n'),
+              new TextSpan(
+                  text: '注意事項：\n',
+                  style: TextStyle(fontWeight: FontWeight.bold, height: 2)),
+              new TextSpan(
+                  text: '1. 請在光源充足的地方進行拍攝\n2. 拍攝時請稍微移動臉部讓相機拍到左右側臉以提升辨識能力\n'),
+              new TextSpan(text: '3. 拍攝結束後照片將會上傳至系統'),
             ],
           ),
         ));
